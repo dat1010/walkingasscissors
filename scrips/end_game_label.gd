678b196e1beec48c8e24577d02c2322609c0422e
David <tannerdjr@gmail.com>
2025-01-25 16:49:04 -0500

    added back story and updated end scene to restart game

--- scrips/end_game_label.gd
+++ scrips/end_game_label.gd
@@ -5,3 +5,7 @@ extends Label
 
 func end_game_high_score():
 	label.text = "You DIED!!!!"
+
+
+func _on_restart_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
--- scrips/main_menu.gd
+++ scrips/main_menu.gd
@@ -12,7 +12,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+	get_tree().change_scene_to_file("res://scenes/backstory.tscn")
 
 
 func _on_tutorial_pressed() -> void:
